af7c8add662c98ba53e6a7f6a31b5f8e991d78eb
Roman <rostr@rambler.ru>
2025-06-24 19:30:50 +0300

    Redis также запускается через  docker compose

--- rest_api/main.go
+++ rest_api/main.go
@@ -16,7 +16,7 @@ func main()  {
 	if err != nil {
 		log.Fatal("Ошибка подключения к реляционной базе данных: ", err)
 	}
-	err = storage.InitNewClient(":6379")
+	err = storage.InitNewClient("redis:6379") // "redis:6379" - compose, "localhost:6379" - go run
 	if err != nil {
 		log.Fatal("Ошибка подключения к Redis: ", err)
 	}
