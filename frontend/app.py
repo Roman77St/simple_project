7a59610cd9d7c3c757870984199b761cab5a38fe
Roman <rostr@rambler.ru>
2025-07-04 17:05:52 +0300

    Изменил отдачу статики на фронтенде.

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,6 +7,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+
+print(STATIC_DIR)
 
 app.add_middleware(
   CORSMiddleware,
@@ -23,7 +28,7 @@ def index():
 def login():
     return FileResponse("templates/login.html")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 # pip install fastapi uvicorn в виртуальной среде
 # запуск сервера: uvicorn app:app --port 8001
